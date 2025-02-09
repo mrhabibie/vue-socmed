1d555a7792007553a6ee8873cb72d2627c6ca87a
Habibie <mrhabibiedev@gmail.com>
2025-02-09 22:24:51 +0700

    add tailwindcss

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,7 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-
+import './style.css'
 import App from './App.vue'
+import { createPinia } from 'pinia'
 import router from './router'
 
 const app = createApp(App)
